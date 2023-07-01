2ecb25296e5558cb1a5065163b21568ab3e41abd
josemls <jose_9513@hotmail.com>
2023-07-01 13:29:04 -0400

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,47 @@
 #!/usr/bin/python3
-"""Gather data from an API."""
-
-
-import requests
-import sys
-
+""" Console Module """
+"""
+Pythin script that uses this  rest api for given
+employee ID to return info about his/her todo list progress
+"""
 if __name__ == '__main__':
+    import csv
+    import json
+    from requests import get
+    from sys import argv
+    """this module is fucking documented"""
 
-    emp_id = sys.argv[1]
+    employee_ID = argv[1]
+    all_todos = get(f'https://jsonplaceholder.typicode.com/todos')
+    users = get(f'https://jsonplaceholder.typicode.com/users')
 
-    api_url = "https://jsonplaceholder.typicode.com"
+    employee_list = json.loads(all_todos.text)
+    users = json.loads(users.text)
+    for user in users:
+        if user.get('id') == int(employee_ID):
+            employee_name = user.get('username')
 
-    url = f"{api_url}/users/{emp_id}/todos"
-    resp = requests.get(url)
-    emp_tasks = resp.json()
+    task_count = 0
+    task_list = []
+    task_success = []
+    for employee in employee_list:
+        if employee.get('userId') == int(employee_ID):
+            task_count += 1
+            task_list.append(employee['title'])
+            task_success.append(employee['completed'])
 
-    url = f"{api_url}/users/{emp_id}"
-    resp = requests.get(url)
-    emp_info = resp.json()
+    csv_list = []
+    employee_id = str(employee_ID)
+    for i in range(len(task_list)):
+        string = (
+            f"['{employee_id}', '{employee_name}',"
+            f" '{task_success[i]}', '{task_list[i]}']"
+        )
+        line = eval(string)
+        csv_list.append(line)
 
-    for task in emp_tasks:
+    with open(f'{employee_ID}.csv', mode='w') as f:
+        f = csv.writer(f, quoting=1)
 
-        output = f'\"{emp_info.get("id")}\",\"{emp_info.get("username")}\",\"\
-            {task.get("completed")}\",\"{task.get("title")}\"\n'
-
-        with open(f"{emp_id}.csv", mode="a", newline="") as f:
-            f.write(output)
+        for list in csv_list:
+            f.writerow(list)
